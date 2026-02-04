c3feb2bfc3a9b6bcb19e06301e44f07d739c8ba0
Qi-Month <dy757410@163.com>
2026-02-04 17:38:33 +0800

    fix

--- kubejs/server_scripts/data/FuelTypes.js
+++ kubejs/server_scripts/data/FuelTypes.js
@@ -1,53 +1,4 @@
 ServerEvents.highPriorityData((event) => {
-	// 植物油
-	new FuelType("#forge:plantoil", 1)
-		.normal(32, 16384, 3)
-		.modular(32, 32768, 5)
-		.huge(48, 65536, 9)
-		.build()
-
-	// 乙醇
-	new FuelType("#forge:ethanol", 1)
-		.normal(48, 16384, 4)
-		.modular(48, 32768, 5)
-		.huge(64, 65536, 6)
-		.build()
-
-	// 生物柴油
-	new FuelType("#forge:biodiesel", 1)
-		.normal(64, 18432, 3)
-		.modular(64, 36864, 5)
-		.huge(96, 73728, 9)
-		.build()
-
-	// 煤油
-	new FuelType("cmi:kerosene", 1)
-		.normal(32, 18432, 6)
-		.modular(32, 36864, 8)
-		.huge(64, 73728, 12)
-		.build()
-
-	// 汽油
-	new FuelType("#forge:gasoline", 1)
-		.normal(96, 18432, 3)
-		.modular(96, 36864, 5)
-		.huge(128, 73728, 8)
-		.build()
-
-	// 柴油
-	new FuelType("#forge:diesel", 1)
-		.normal(128, 20480, 6)
-		.modular(128, 40960, 8)
-		.huge(128, 81920, 12)
-		.build()
-
-	// 精炼油
-	new FuelType("thermal:refined_fuel", 1)
-		.normal(128, 20480, 4)
-		.modular(128, 40960, 6)
-		.huge(128, 81920, 8)
-		.build()
-
 	/**
 	 * 
 	 * @param {String} name 流体ID(Tag)
@@ -138,4 +89,53 @@ ServerEvents.highPriorityData((event) => {
 		event.addJson(`cmi:railways_liquid_fuel/${this.getFluidName}.json`, trainFuelData)
 		return this
 	}
+
+	// 植物油
+	new FuelType("#forge:plantoil", 1)
+		.normal(32, 16384, 3)
+		.modular(32, 32768, 5)
+		.huge(48, 65536, 9)
+		.build()
+
+	// 乙醇
+	new FuelType("#forge:ethanol", 1)
+		.normal(48, 16384, 4)
+		.modular(48, 32768, 5)
+		.huge(64, 65536, 6)
+		.build()
+
+	// 生物柴油
+	new FuelType("#forge:biodiesel", 1)
+		.normal(64, 18432, 3)
+		.modular(64, 36864, 5)
+		.huge(96, 73728, 9)
+		.build()
+
+	// 煤油
+	new FuelType("cmi:kerosene", 1)
+		.normal(32, 18432, 6)
+		.modular(32, 36864, 8)
+		.huge(64, 73728, 12)
+		.build()
+
+	// 汽油
+	new FuelType("#forge:gasoline", 1)
+		.normal(96, 18432, 3)
+		.modular(96, 36864, 5)
+		.huge(128, 73728, 8)
+		.build()
+
+	// 柴油
+	new FuelType("#forge:diesel", 1)
+		.normal(128, 20480, 6)
+		.modular(128, 40960, 8)
+		.huge(128, 81920, 12)
+		.build()
+
+	// 精炼油
+	new FuelType("thermal:refined_fuel", 1)
+		.normal(128, 20480, 4)
+		.modular(128, 40960, 6)
+		.huge(128, 81920, 8)
+		.build()
 })
