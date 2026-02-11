3dedf006f575d2efd8dbec6751f15bae5de77b74
Eternal-Snowstorm <3512546747@qq.com>
2026-02-11 20:00:01 +0100

    一点修改

--- kubejs/server_scripts/recipes/mod/create/Fermenting.js
+++ kubejs/server_scripts/recipes/mod/create/Fermenting.js
@@ -4,7 +4,7 @@ ServerEvents.recipes((event) => {
 	let biodiesel = Fluid.of("createdieselgenerators:biodiesel")
 
 	// 生物柴油
-	createdieselgenerators.basin_fermenting(Fluid.of("createdieselgenerators:biodiesel", 100), [
+	createdieselgenerators.basin_fermenting(biodiesel.withAmount(100), [
 		"8x minecraft:sugar",
 		Fluid.of("immersiveengineering:creosote", 100)
 	]).processingTime(20 * 10).heatRequirement("grilled")
