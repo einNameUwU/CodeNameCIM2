791a20da90ed1004a718f1d7c0c59df4c6df9d08
Qi-Month <dy757410@163.com>
2026-02-14 09:20:23 +0800

    怎么还多个方法

--- kubejs/startup_scripts/register/item/Molds.js
+++ kubejs/startup_scripts/register/item/Molds.js
@@ -2,7 +2,6 @@ StartupEvents.registry("item", (event) => {
     function addMold(name) {
         let builder = event.create(`${global.namespace}:${name}_mold`)
 
-        builder.tag("vintageimprovements:curving_heads")
         builder.texture(`${global.namespace}:item/material/mold/${name}`)
         builder.tag("vintageimprovements:curving_heads")
         builder.tag("cmi:molds")
