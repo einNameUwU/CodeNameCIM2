186bd09fa985c32156c27c1036a445fec3a6c61e
Qi-Month <dy757410@163.com>
2026-02-17 15:24:49 +0800

    转移

---
+++ kubejs/server_scripts/recipes/mod/create/machine/Copper.js
@@ -0,0 +1,66 @@
+ServerEvents.recipes((event) => {
+	let { kubejs } = event.recipes
+	const MECH = Mechanism.WOODEN.COM
+	const CASING = "create:copper_casing"
+
+	// 流体元件外壳
+	kubejs.shaped("ae2:fluid_cell_housing", [
+		"XYX",
+		"YMY",
+		"XYX"
+	], {
+		M: MECH,
+		X: "#forge:plates/copper",
+		Y: "ae2:quartz_glass"
+	}).id("ae2:network/cells/fluid_cell_housing")
+
+	// 移动式流体接口
+	kubejs.shapeless("create:portable_fluid_interface", [CASING,
+		MECH,
+		"#forge:plates/industrial_iron"
+	]).id("create:crafting/kinetics/portable_fluid_interface")
+
+	// 分液池
+	kubejs.shaped("create:item_drain", [
+		"X",
+		"M",
+		"C"
+	], {
+		M: MECH,
+		X: "minecraft:iron_bars",
+		C: CASING
+	}).id("create:crafting/kinetics/item_drain")
+
+	// 注液器
+	kubejs.shaped("create:spout", [
+		"M",
+		"C",
+		"X"
+	], {
+		M: MECH,
+		X: "thermal:cured_rubber",
+		C: CASING
+	}).id("create:crafting/kinetics/spout")
+
+	// 软管滑轮
+	kubejs.shaped("create:hose_pulley", [
+		"C",
+		"X",
+		"M"
+	], {
+		M: MECH,
+		X: "thermal:cured_rubber_block",
+		C: CASING
+	}).id("create:crafting/kinetics/hose_pulley")
+
+	// // 灌溉塔
+	// kubejs.shaped("create:hose_puyylley", [
+	//     "AMA",
+	//     "BCB"
+	// ], {
+	//     M: MECH,
+	//     A: "#forge:plates/copper",
+	//     B: "#forge:rods/copper",
+	//     C: "create:fluid_pipe"
+	// }).id("create:crafting/kinetics/ley")
+})
---
+++ kubejs/server_scripts/recipes/mod/create/machine/Wooden.js
@@ -0,0 +1,63 @@
+ServerEvents.recipes((event) => {
+	let { kubejs } = event.recipes
+	const MECH = Mechanism.WOODEN.COM
+
+	// 漏斗
+	kubejs.shaped("minecraft:hopper", [
+		"X X",
+		"XMX",
+		" X "
+	], {
+		M: MECH,
+		X: ["#forge:plates/tin", "#forge:plates/iron"],
+	}).id("minecraft:hopper")
+
+	// 溜槽
+	kubejs.shaped("2x create:chute", [
+		"X",
+		"Y"
+	], {
+		X: "#forge:ingots/industrial_iron",
+		Y: "#forge:plates/industrial_iron",
+	}).id("create:crafting/kinetics/chute")
+
+	// 背包
+	// !!!
+	kubejs.shaped("sophisticatedbackpacks:backpack", [
+		"XYX",
+		"XMX",
+		"YYY"
+	], {
+		X: "#forge:string",
+		Y: ["#forge:leather", "tconstruct:silky_cloth"]
+	}).id("sophisticatedbackpacks:backpack")
+
+	// 空白升级
+	// !!!
+	kubejs.shaped("sophisticatedbackpacks:upgrade_base", [
+		"XYX",
+		"YMY",
+		"XYX"
+	], {
+		X: "#forge:rods/andesite",
+		Y: ["#forge:leather", "tconstruct:silky_cloth"]
+	}).id("sophisticatedbackpacks:upgrade_base")
+
+	// 移动式存储接口
+	kubejs.shapeless("create:portable_storage_interface", [
+		"create:andesite_casing",
+		MECH,
+		"#forge:plates/industrial_iron"
+	]).id("create:crafting/kinetics/portable_storage_interface")
+
+	// ME物品原件外壳
+	// !!!
+	kubejs.shaped("2x ae2:item_cell_housing", [
+		"XYX",
+		"YMY",
+		"XYX"
+	], {
+		X: "#forge:plates/silver",
+		Y: "ae2:quartz_glass",
+	}).id("ae2:network/cells/item_cell_housing")
+})
