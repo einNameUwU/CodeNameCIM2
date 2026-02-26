37b6a197a6aeb6417a2ad0c4a5383c8c2fe9e503
Qi-Month <dy757410@163.com>
2026-02-26 14:22:41 +0800

    删配方

--- kubejs/server_scripts/recipes/mod/machines/FlashDiskWriter.js
+++ kubejs/server_scripts/recipes/mod/machines/FlashDiskWriter.js
@@ -213,40 +213,4 @@ ServerEvents.recipes((event) => {
 			]
 		})
 	}
-
-	// Citron
-	let recipes = [
-		["#cmi:torch", "torcherino:torcherino", 1, 2],
-		["#cmi:torch", "torcherino:compressed_torcherino", 3, 4],
-		["#cmi:torch", "torcherino:double_compressed_torcherino", 5, 6],
-		["#cmi:lantern", "torcherino:lantern", 1, 2],
-		["#cmi:lantern", "torcherino:compressed_lantern", 3, 4],
-		["#cmi:lantern", "torcherino:double_compressed_lantern", 5, 6],
-		["#cmi:pumpkin", "torcherino:lanterino", 1, 2],
-		["#cmi:pumpkin", "torcherino:compressed_lanterino", 3, 4],
-		["#cmi:pumpkin", "torcherino:double_compressed_lanterino", 5, 6]
-	]
-	recipes.forEach((value) => {
-		event.custom({
-			type: "custommachinery:custom_machine",
-			machine: `${global.namespace}:flash_disk_writer`,
-			time: 400,
-			hidden: false,
-			requirements: [
-				{ type: "custommachinery:item", mode: "input", item: "minecraft:clock", amount: 64 },
-				{ type: "custommachinery:item", mode: "input", item: "minecraft:clock", amount: 64 },
-				{ type: "custommachinery:item", mode: "input", item: "minecraft:clock", amount: 64 },
-				{ type: "custommachinery:item", mode: "input", item: "minecraft:clock", amount: 64 },
-				{ type: "custommachinery:item", mode: "input", item: value[0], amount: 1 },
-				{ type: "custommachinery:item", mode: "output", item: value[1], amount: 1 },
-				{ type: "custommachinery:energy", mode: "input", amount: 114514 },
-				{
-					type: "custommachinery:position",
-					x: ClientSeedHandler.getValue(value[2]).toString(),
-					y: "*",
-					z: ClientSeedHandler.getValue(value[3]).toString()
-				}
-			]
-		}).id(value[1])
-	})
 })
