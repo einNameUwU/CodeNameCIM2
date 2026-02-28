a0d30c59959e99b74d767ea022430c6d0647dedb
Qi-Month <dy757410@163.com>
2026-02-28 14:43:59 +0800

    哦草这个没提交

--- kubejs/server_scripts/event/entity/attribute/Health.js
+++ kubejs/server_scripts/event/entity/attribute/Health.js
@@ -2,12 +2,11 @@ EntityEvents.spawned((event) => {
 	let { entity } = event
 
 	/**
-	 * @type {{ [key: Special.EntityType]: ResourceLocation_ }}
+	 * @type {{ [key: Special.EntityType]: number }}
 	 */
-	let customHealth = Object.freeze({
-		"minecraft:iron_golem": 1024,
+	let customHealth = {
 		"minecraft:ender_dragon": 2000
-	})
+	}
 	let value = customHealth[entity.getType()]
 
 	if (!entity.isLiving()) {
