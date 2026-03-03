457c0fcf3cb0ec2f3974bdafa1ecc80f6a02620c
Qi-Month <dy757410@163.com>
2026-03-03 23:55:10 +0800

    改一下

--- kubejs/server_scripts/recipes/process/TreeBarkProcess.js
+++ kubejs/server_scripts/recipes/process/TreeBarkProcess.js
@@ -1,91 +1,78 @@
 ServerEvents.recipes((event) => {
-    let { farmersdelight, createdieselgenerators, create, minecraft, kubejs } = event.recipes
+	let { farmersdelight, createdieselgenerators, create, minecraft, kubejs } = event.recipes
 
-    // 树皮切木屑
-    farmersdelight.cutting("createdieselgenerators:wood_chip",
-        "#cmi:tree_barks",
-        "#minecraft:axes"
-    )
-    create.cutting("createdieselgenerators:wood_chip",
-        "#cmi:tree_barks"
-    )
+	// 树皮切木屑
+	farmersdelight.cutting("createdieselgenerators:wood_chip", [
+		"#cmi:tree_barks"
+	], "#minecraft:axes")
+	create.cutting("createdieselgenerators:wood_chip", [
+		"#cmi:tree_barks"
+	])
 
-    // 松香
-    farmersdelight.cooking("thermal:rosin",
-        [
-            "#cmi:tree_barks",
-            "#cmi:tree_barks",
-            "#cmi:tree_barks",
-            "#forge:rods/wooden"
-        ])
+	// 松香
+	farmersdelight.cooking("thermal:rosin", [
+		"#cmi:tree_barks",
+		"#cmi:tree_barks",
+		"#cmi:tree_barks",
+		"#forge:rods/wooden"
+	])
 
-    // 防腐木板
-    kubejs.shaped("4x immersiveengineering:treated_wood_horizontal", [
-        " A ",
-        "ABA",
-        " A "
-    ], {
-        A: "#minecraft:planks",
-        B: "thermal:rosin"
-    }).id("immersiveengineering:crafting/treated_wood_horizontal")
+	// 防腐木板
+	kubejs.shaped("4x immersiveengineering:treated_wood_horizontal", [
+		" A ",
+		"ABA",
+		" A "
+	], {
+		A: "#minecraft:planks",
+		B: "thermal:rosin"
+	}).id("immersiveengineering:crafting/treated_wood_horizontal")
 
-    // 生物质
-    create.mixing("createaddition:biomass",
-        [
-            "3x #cmi:tree_barks",
-            Fluid.of("createdieselgenerators:plant_oil", 100)
-        ]).heated()
+	// 生物质
+	create.mixing("createaddition:biomass", [
+		"3x #cmi:tree_barks",
+		Fluid.of("createdieselgenerators:plant_oil", 100)
+	]).heated()
 
-    // 草木灰
-    minecraft.smelting("cmi:plant_ash",
-        "#createaddition:plants"
-    )
-    minecraft.smelting("cmi:plant_ash",
-        "#minecraft:flowers"
-    )
-    minecraft.smelting("cmi:plant_ash",
-        "#cmi:tree_barks"
-    )
-    minecraft.smelting("cmi:plant_ash",
-        "#minecraft:leaves"
-    )
-    minecraft.smelting("cmi:plant_ash",
-        "#forge:dusts/wood"
-    )
+	// 草木灰
+	minecraft.smelting("cmi:plant_ash", "#createaddition:plants")
+	minecraft.smelting("cmi:plant_ash", "#minecraft:flowers")
+	minecraft.smelting("cmi:plant_ash", "#cmi:tree_barks")
+	minecraft.smelting("cmi:plant_ash", "#minecraft:leaves")
+	minecraft.smelting("cmi:plant_ash", "#forge:dusts/wood")
 
-    // 有机肥料
-    kubejs.shapeless("farmersdelight:organic_compost", [
-        "minecraft:dirt",
-        "#cmi:tree_barks",
-        "cmi:plant_ash",
-        "#forge:fuels/bio"
-    ]).id("farmersdelight:organic_compost_from_tree_bark")
-    kubejs.shapeless("farmersdelight:organic_compost", [
-        "minecraft:dirt",
-        "minecraft:rotten_flesh",
-        "cmi:plant_ash",
-        "#forge:fuels/bio"
-    ]).id("farmersdelight:organic_compost_from_rotten_flesh")
+	// 有机肥料
+	kubejs.shapeless("farmersdelight:organic_compost", [
+		"minecraft:dirt",
+		"#cmi:tree_barks",
+		"cmi:plant_ash",
+		"#forge:fuels/bio"
+	]).id("farmersdelight:organic_compost_from_tree_bark")
+	kubejs.shapeless("farmersdelight:organic_compost", [
+		"minecraft:dirt",
+		"minecraft:rotten_flesh",
+		"cmi:plant_ash",
+		"#forge:fuels/bio"
+	]).id("farmersdelight:organic_compost_from_rotten_flesh")
 
-    // 堆肥
-    createdieselgenerators.basin_fermenting("thermal:compost", [
-        "#cmi:tree_barks",
-        "cmi:plant_ash",
-        "#forge:fuels/bio"
-    ])
-    createdieselgenerators.basin_fermenting("thermal:compost", [
-        "minecraft:rotten_flesh",
-        "cmi:plant_ash",
-        "#forge:fuels/bio"
-    ])
+	// 堆肥
+	createdieselgenerators.basin_fermenting("thermal:compost", [
+		"#cmi:tree_barks",
+		"cmi:plant_ash",
+		"#forge:fuels/bio"
+	])
+	createdieselgenerators.basin_fermenting("thermal:compost", [
+		"minecraft:rotten_flesh",
+		"cmi:plant_ash",
+		"#forge:fuels/bio"
+	])
 
-    // 沃土
-    create.item_application("farmersdelight:rich_soil", [
-        "minecraft:dirt",
-        "thermal:compost"
-    ])
-    create.item_application("farmersdelight:rich_soil_farmland", [
-        "minecraft:farmland",
-        "thermal:compost"
-    ])
+	// 沃土
+	create.item_application("farmersdelight:rich_soil", [
+		"minecraft:dirt",
+		"thermal:compost"
+	])
+	create.item_application("farmersdelight:rich_soil_farmland", [
+		"minecraft:farmland",
+		"thermal:compost"
+	])
 })
