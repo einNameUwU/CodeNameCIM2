2a3d636e16ce6b7865837020ae5947993c8b222a
Qi-Month <dy757410@163.com>
2026-03-06 16:45:14 +0800

    按理说不需要toString()

--- kubejs/server_scripts/recipes/mechanism/ComponentAssembly.js
+++ kubejs/server_scripts/recipes/mechanism/ComponentAssembly.js
@@ -2,7 +2,7 @@ ServerEvents.recipes((event) => {
     let { thermal_extra } = event.recipes
 
     // 热力组件
-    thermal_extra.component_assembly(`4x ${Mechanism.THERMAL.AUG.getId().toString()}`, [
+    thermal_extra.component_assembly(`4x ${Mechanism.THERMAL.AUG.getId()}`, [
         "#forge:gears/constantan",
         "thermal:rf_coil",
         "thermal:redstone_servo",
@@ -10,21 +10,21 @@ ServerEvents.recipes((event) => {
     ])
 
     // 轻工构件
-    thermal_extra.component_assembly(`4x ${Mechanism.LIGHT.AUG.getId().toString()}`, [
+    thermal_extra.component_assembly(`4x ${Mechanism.LIGHT.AUG.getId()}`, [
         "#forge:gears/copper",
         "immersiveengineering:component_iron",
         "cmi:diode"
     ])
 
     // 智能构件
-    thermal_extra.component_assembly(`4x ${Mechanism.SMART.AUG.getId().toString()}`, [
+    thermal_extra.component_assembly(`4x ${Mechanism.SMART.AUG.getId()}`, [
         "#forge:gears/silver",
         "create_connected:control_chip",
         "ae2:logic_processor"
     ])
 
     // 强化构件
-    thermal_extra.component_assembly(`4x ${Mechanism.REINFORCED.AUG.getId().toString()}`, [
+    thermal_extra.component_assembly(`4x ${Mechanism.REINFORCED.AUG.getId()}`, [
         "#forge:gears/signalum",
         "thermal:rf_coil",
         "cmi:thermal_unit",
@@ -32,7 +32,7 @@ ServerEvents.recipes((event) => {
     ])
 
     // 重工构件
-    thermal_extra.component_assembly(`4x ${Mechanism.HEAVY.AUG.getId().toString()}`, [
+    thermal_extra.component_assembly(`4x ${Mechanism.HEAVY.AUG.getId()}`, [
         "#forge:gears/electrum",
         "immersiveengineering:component_steel",
         "immersiveengineering:electron_tube"
