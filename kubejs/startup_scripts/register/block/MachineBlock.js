c9690fe7c64f0361c43659ed6d826ce374e212d4
Qi-Month <dy757410@163.com>
2026-04-22 21:45:17 +0800

    global

--- kubejs/startup_scripts/register/block/MachineBlock.js
+++ kubejs/startup_scripts/register/block/MachineBlock.js
@@ -1,47 +1,5 @@
 StartupEvents.registry("block", (event) => {
-	let machineBlockRegister = [
-		"nature",
-		"wooden",
-		"stone",
-		"iron",
-		"copper",
-		"andesite",
-		"photosensitive",
-		"gold",
-		"cobalt",
-		"nether",
-		"thermal",
-		"reinforced",
-		"resonant",
-		"railway",
-		"ender",
-		"light_engineering",
-		"heavy_engineering",
-		"enchanted",
-		"smart",
-		"computing",
-		"tier_1_aviation",
-		"tier_2_aviation",
-		"tier_3_aviation",
-		"tier_4_aviation",
-		"basic_mekanism",
-		"advanced_mekanism",
-		"elite_mekanism",
-		"ultimate_mekanism",
-		"air_tight",
-		"nuclear",
-		"antimatter",
-		"coil",
-		"sculk",
-		"colorful",
-		"creative",
-		"potion",
-		"precision",
-		"redstone",
-		"bronze",
-		"pig_iron"
-	]
-	machineBlockRegister.forEach((type) => {
+	CmiGlobal.mechanismGroup.forEach((type) => {
 		event.create(`${CmiCore.MODID}:${type}_machine`, "cardinal")
 			.soundType(SoundType.METAL)
 			.waterlogged()
