e49e65e2b8ef12193aa96dc74a0f0321b9747820
Qi-Month <dy757410@163.com>
2026-05-07 01:41:05 +0800

    fix

--- kubejs/server_scripts/event/functional/client/LoggedIn.js
+++ kubejs/server_scripts/event/functional/client/LoggedIn.js
@@ -1,6 +1,14 @@
+ServerEvents.loaded((event) => {
+	let { server } = event
+
+	server.scheduleInTicks(2, () => {
+		server.runCommandSilent("reload")
+	})
+})
+
 PlayerEvents.loggedIn((event) => {
 	let { player } = event
 
 	let tranKey = `message.${Cmi.MODID}.welcome`
-	player.tell(Component.translatable(tranKey, player.username))
+	player.tell(Component.translatable(tranKey, player.getUsername()))
 })
