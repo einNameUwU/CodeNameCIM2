fbf9b08320724cd878ab43dc71eec7bf3040b351
Eternal-Snowstorm <eternalsnowstorm@foxmail.com>
2026-05-07 13:24:35 +0800

    删了个失效配方

--- kubejs/server_scripts/recipes/mod/cmi/Freezing.js
+++ kubejs/server_scripts/recipes/mod/cmi/Freezing.js
@@ -1,11 +1,6 @@
 ServerEvents.recipes((event) => {
 	let { cmi } = event.getRecipes()
 
-	// 巧克力
-	cmi.freezing("ratatouille:chocolate_mold_solid", [
-		"ratatouille:chocolate_mold_filled"
-	])
-
 	// 浮冰
 	cmi.freezing("minecraft:packed_ice", [
 		"minecraft:ice"
