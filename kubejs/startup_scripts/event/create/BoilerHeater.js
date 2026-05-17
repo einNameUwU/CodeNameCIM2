eb5e840bce29f36d0df144d5143e86c35daed108
Qi-Month <dy757410@163.com>
2026-05-17 21:13:46 +0800

    我懒得测试了

---
+++ kubejs/startup_scripts/event/create/BoilerHeater.js
@@ -0,0 +1,13 @@
+let $BlockStateProperties =
+	Java.loadClass("net.minecraft.world.level.block.state.properties.BlockStateProperties")
+
+CreateEvents.boilerHeatHandler((event) => {
+	event.add("cmi:bronze_fluid_burner", (block) => {
+		let state = block.getBlockState()
+
+		if (state.getValue($BlockStateProperties.LIT)) {
+			return 2
+		}
+		return 1
+	})
+})
