fac08af1921a008374ebe846eccc116d15671593
Qi-Month <dy757410@163.com>
2026-06-19 11:19:58 +0800

    mold recipe

--- kubejs/server_scripts/recipes/mod/create/Casting.js
+++ kubejs/server_scripts/recipes/mod/create/Casting.js
@@ -20,19 +20,42 @@ function castingRecipes(event) {
  * @param {Internal.RecipesEventJS} event
  */
 function moldRecipes(event) {
-	Object.values(CDGMolds).forEach((group) => {
-		Object.values(group).forEach((moldId) => {
-			event.custom({
-				type: "minecraft:stonecutting",
-				count: 1,
-				ingredient: Ingredient.of("#forge:plates/industrial_iron").toJson(),
-				result: {
-					item: "createdieselgenerators:mold",
-					nbt: {
-						Mold: moldId
-					}
-				}
-			})
-		})
+	let { vintageimprovements, kubejs } = event.getRecipes()
+
+	addMoldRecipe(CDGMolds.CMI.PLATE, "tconstruct:plate_cast")
+	addMoldRecipe(CDGMolds.CDG.BAR, "tconstruct:rod_cast")
+	addMoldRecipe(CDGMolds.CDG.BOWL, "minecraft:bowl")
+	addMoldRecipe(CDGMolds.CDG.CHAIN, "minecraft:chain")
+	addMoldRecipe(CDGMolds.CDG.LINES, "minecraft:iron_ingot")
+
+	kubejs.shaped("createdieselgenerators:mold", [
+		"AA",
+		"AA"
+	], {
+		A: "#forge:plates/industrial_iron"
 	})
+
+	/**
+	 * 
+	 * @param {CDGMolds} id 
+	 * @returns 
+	 */
+	function mold(id) {
+		return Item.of("createdieselgenerators:mold")
+			.withNBT({
+				Mold: id
+			})
+	}
+
+	/**
+	 * 
+	 * @param {CDGMolds} moldId 
+	 * @param {Internal.ItemStack_} input 
+	 * @returns 
+	 */
+	function addMoldRecipe(moldId, input) {
+		return vintageimprovements.curving(mold(moldId), [
+			"createdieselgenerators:mold"
+		]).itemAsHead(input)
+	}
 }
